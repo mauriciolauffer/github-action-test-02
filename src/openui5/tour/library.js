d0bb91c6eb45ec47537c20dc025381b4201abeff
mauriciolauffer <mauriciolauffer@users.noreply.github.com>
2021-04-06 01:33:50 +0000

    ci: Build and publish BETA

---
+++ src/openui5/tour/library.js
@@ -0,0 +1,33 @@
+/*
+ * ${copyright}
+ */
+// comment x3
+sap.ui.define([
+  'sap/ui/core/library',
+  'sap/m'
+], function() {
+  'use strict';
+
+  /**
+   * OpenUI5 library: openui5.tour
+   *
+   * @namespace
+   * @name openui5.tour
+   * @author Mauricio Lauffer
+   * @version ${version}
+   * @public
+   */
+  return sap.ui.getCore().initLibrary({
+    name: 'openui5.tour',
+    dependencies: [
+      'sap.ui.core',
+      'sap.m'
+    ],
+    controls: [
+      'openui5.tour.Tour',
+      'openui5.tour.TourStep'
+    ],
+    noLibraryCSS: true,
+    version: '${version}'
+  });
+});
